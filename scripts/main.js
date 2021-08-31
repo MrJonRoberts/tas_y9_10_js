037bd6a14d90d71804a7abf4d4e593a7579e22ba
MrJonRoberts <jroberts@tas.qld.edu.au>
2021-08-31 11:43:45 +1000

    assign variables

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,32 @@
 console.log("javascript enabled");
+
+
+// alert("JS is Working");
+
+console.log("JS is Working");
+
+msgElt = document.getElementById("msg");
+
+msgElt.innerHTML = "<h2>JS is Working!!!</h2>";
+
+
+// this is a comment.
+// # is a comment in python
+
+// variables
+// PYTHON :    name = "Fred"
+
+let name = "Fred";   // constant - cannot change the value
+var age = 21;
+
+// create a string
+var dayOfWeek = "Tuesday";  // a bunch of characters inside quotes.
+
+// create a integer
+var numberOfEyes = 2;
+
+// create a float:
+var tempInDegC = 28.5;
+
+// create a boolean:
+var isItRaining = true;   // in python Booleans are True / False.
